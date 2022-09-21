f5958f055585ca57cf96a28145b3b1189b173905
KHW2019 <gabrielwang5354@outlook.com>
2022-09-21 17:17:06 +0100

    Added PauseMenu

---
+++ Ui Development/Assets/Script/Menu/InGameMenu/PauseMenu.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    
+    [Header("Reference")]
+    public GameObject pauseMenuUI;
+
+    [Header("Input")]
+    public KeyCode pauseGameTirgger = KeyCode.Escape;
+
+    private static bool isPause = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PauseMenuStatus();
+    }
+
+    void PauseMenuStatus()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPause = false;
+        CusorLocked();
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPause = true;
+        CusorUnlocked();
+    }
+
+    void CusorUnlocked()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    void CusorLocked()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void backToMenu(int sceneIndex)
+    {
+        SceneManager.LoadSceneAsync(sceneIndex);
+        Time.timeScale = 1f;
+    }
+}
